738c09b9d20c465f1d6f9c898bd308d77527ae79
Volodymyr <vavan123music@gmail.com>
2024-09-24 23:16:43 +0300

    added config file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_ID = process.env.BOT_ID;
+const SERVER_ID = process.env.SERVER_ID;
+
+module.exports = {
+    BOT_TOKEN,
+    BOT_ID,
+    SERVER_ID
+};
